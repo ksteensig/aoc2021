0c58fe29c169ce27a37d6f673ba9242d6f9f1176
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2021-12-01 20:17:56 +0100

    shorten further

--- day1.hs
+++ day1.hs
@@ -1,6 +1,5 @@
 import System.IO ( openFile )
 import Data.List ( tails, transpose )
-
 import AOC2021 ( load, readInt ) 
 
 parse = map readInt . words
@@ -10,10 +9,11 @@ main = do
         print $ part1 list
         print $ part2 list
 
+differences fs = zipWith (-) fs $ tail fs
+
+windows n = Data.List.transpose . take n . tails
+
 part1 = length . filter (< 0) . differences
 
 part2 = part1 . map sum . windows 3
 
-differences fs = zipWith (-) fs $ tail fs
-
-windows n xs = Data.List.transpose . take n $ tails xs
